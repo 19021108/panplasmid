68eb071ee6bf0e5018326ed6e63f5247372c42a4
Superman <56293809+19021108@users.noreply.github.com>
2024-04-15 10:18:05 +0000

    Create environment and scripts

---
+++ script.sh
@@ -0,0 +1,8 @@
+# mkdir data
+# mkdir models
+
+# git clone 'https://github.com/fmfi-compbio/plasgraph2-datasets' '/workspaces/panplasmid/data/plasgraph2_datasets'
+
+conda env export > environment.yml
+
+# git clone https://github.com/cchauve/plASgraph2 '/workspaces/panplasmid/models/plasgraph2'
